94dd1493e170850ca0a2cec07f1dc2f64dfd42ba
Michal Pluta <vscd48@durham.ac.uk>
2024-04-11 18:17:49 +0100

    Get monthly market cap data

--- scraping/market_cap.py
+++ scraping/market_cap.py
@@ -1,14 +1,9 @@
+from datetime import datetime
+import json
+import re
 import requests as rq
 from bs4 import BeautifulSoup
 import pandas as pd
-from decimal import Decimal
-
-units = {
-    'T': int(1e12),
-    'B': int(1e9),
-    'M': int(1e6),
-    'K': int(1e3)
-}
 
 URL = 'https://companiesmarketcap.com/%s/marketcap/'
 OUTPUT_UNIT = 'M'
@@ -45,53 +40,35 @@ names = {
     "COKE": "coca-cola-consolidated",
 }
 
-
-def convert_market_cap_value(input: str) -> str:
-    # Extract unit and numeric value
-    unit = input[-1]
-    str_market_cap = input[1:-2]
-
-    # Convert to raw unit value
-    value = Decimal(str_market_cap) * units[unit]
-
-    # Convert to output unit
-    return str(value / units[OUTPUT_UNIT])
-
-
 def get_data(ticker):
     # Make a response to the website
     response = rq.get(URL % names[ticker])
-
+    
     # Get the soup
     soup = BeautifulSoup(response.text, 'html.parser')
-    table = soup.find('table')
+    
+    # Find the underlying data in the html script tag
+    script_tag = soup.find('script', text=re.compile('data = \['))
+    data_string = re.search('data = (\[.*?\]);', script_tag.string).group(1)
 
-    # Store data for the current ticker
+    # Parse the string as JSON to convert to a Python list
+    raw_data = json.loads(data_string)
+    
     data = []
-    for row in table.find_all('tr'):
-        # Find all cells on the row
-        cells = row.find_all('td')
-
-        # Check if empty row
-        if not cells:
+    for item in raw_data:
+        # Convert timestamp to date and format it
+        date = datetime.fromtimestamp(item['d'])
+        year_month = f"{date.year}-{str(date.month).rjust(2,'0')}"
+        
+        # Only include the first value of each month
+        if data and year_month == data[-1][0]:
             continue
-
-        # Extract values
-        texts = [cell.text.strip() for cell in cells]
-        year = texts[0]
-        market_cap = texts[1]
-
-        # Check if data point is useful
-        if int(year) < START_YEAR:
-            continue
-
-        # Convert market cap to a standard unit
-        market_cap = convert_market_cap_value(market_cap)
-
-        # Add to output list
-        data.append([year, ticker, market_cap])
-
-    # Return data
+        
+        # Extract market cap
+        market_cap = int(item['m']) * 100_000
+        
+        data.append([year_month, ticker, market_cap])
+    
     return data
 
 
@@ -106,6 +83,5 @@ if __name__ == '__main__':
         data += get_data(ticker)
 
     # Convert to DataFrame and output to csv
-    df = pd.DataFrame(data, columns=['year', 'ticker',
-                                     'market_cap_' + OUTPUT_UNIT])
+    df = pd.DataFrame(data, columns=['YearMonth', 'Ticker', 'MarketCap'])
     df.to_csv(OUTPUT_DIR, index=False)
