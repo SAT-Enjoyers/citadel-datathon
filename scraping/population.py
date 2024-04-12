faca27e8643660c7002c92795c1ecc5beae62ebe
Michal Pluta <vscd48@durham.ac.uk>
2024-04-12 17:31:03 +0100

    Scrape the population of each state by year

---
+++ scraping/population.py
@@ -0,0 +1,51 @@
+import requests as rq
+import pandas as pd
+
+URL = '''https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0
+&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450
+&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1318&nt=0
+&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id={}POP
+&scale=left&cosd=1900-01-01&coed=2023-01-01&line_color=%234572a7
+&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999
+&oet=99999&mma=0&fml=a&fq=Annual&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1
+&transformation=lin&vintage_date=2024-04-12&revision_date=2024-04-12
+&nd=1900-01-01'''
+
+OUTPUT_DIR = 'udataset/population_by_state.csv'
+START_YEAR = 1900
+
+
+def get_abbreviations():
+    df_abbrev = pd.read_csv("geo/state_abbreviations.csv")[:-5]
+    return df_abbrev.set_index('Abbreviation')['State'].to_dict()
+
+
+def get_data(state_code: str, state_name: str):
+    # Make a response to the website
+    response = rq.get(URL.format(state_code))
+
+    # Turn response into list of lists
+    data = response.text.split('\n')
+    data = [item.split(',') for item in data][1:-1]
+
+    # Filter by year range
+    data = [item for item in data if START_YEAR <= int(item[0][:4])]
+
+    # Format raw data in row into the table
+    data = [[state_name.lower(), item[0][:4], int(float(item[1]) * 1000)]
+            for item in data]
+
+    return data
+
+
+if __name__ == '__main__':
+    abbreviations = get_abbreviations()
+    data = []
+
+    for state, state_full_name in abbreviations.items():
+        print(state_full_name)
+        data += get_data(state, state_full_name)
+
+    df = pd.DataFrame(data=data,
+                      columns=['State', 'Year', 'Population'])
+    df.to_csv(OUTPUT_DIR, index=False)
